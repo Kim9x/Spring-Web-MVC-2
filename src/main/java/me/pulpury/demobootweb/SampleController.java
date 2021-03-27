bab309cad08c9ac4d05713b03b56f2ab841c704e
Kim9x <king960105@gmail.com>
2021-03-27 12:53:13 +0900

    Handler Interceptor 구현
    Handler Interceptor 구현

--- src/main/java/me/pulpury/demobootweb/SampleController.java
+++ src/main/java/me/pulpury/demobootweb/SampleController.java
@@ -8,6 +8,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SampleController {
 	
+	// preHandle 1
+	// preHandle 2
+	// 요청 처리
+	
+	// 역순으로 호출이 된다.  자료형 중에 Stack을 생각하면 될 듯?
+	// postHandler 2
+	// postHandler 1
+	// 뷰 렌더링
+	// afterCompletion 2
+	// afterCompletion 1
+	
 //	@GetMapping("/hello/{name}")
 //	public String hello(@PathVariable("name") Person person) {
 	@GetMapping("/hello")
