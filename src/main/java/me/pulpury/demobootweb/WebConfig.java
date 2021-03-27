ea26d39b950dc657a59a303782c7e3b43386abf2
김태주 <kim9xd@gmail.com>
2021-03-27 14:24:00 +0900

    리소스 핸들러

--- src/main/java/me/pulpury/demobootweb/WebConfig.java
+++ src/main/java/me/pulpury/demobootweb/WebConfig.java
@@ -1,28 +1,45 @@
 package me.pulpury.demobootweb;
 
+import java.util.concurrent.TimeUnit;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.http.CacheControl;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+//	@Override
+//	public void addInterceptors(InterceptorRegistry registry) {
+//		// order 값으로 순서를 지정할 수 있음.
+//		registry.addInterceptor(new GreetingInterceptor()).order(0);
+//		
+//		// addPathPatterns로 특 패턴으로 지정할 수 있음 
+//		registry.addInterceptor(new AnotherInterceptor())
+//				.addPathPatterns("/hi/**")
+//				.order(-1);
+//	}
+
 	@Override
-	public void addInterceptors(InterceptorRegistry registry) {
-		// order 값으로 순서를 지정할 수 있음.
-		registry.addInterceptor(new GreetingInterceptor()).order(0);
-		
-		// addPathPatterns로 특 패턴으로 지정할 수 있음 
-		registry.addInterceptor(new AnotherInterceptor())
-				.addPathPatterns("/hi/**")
-				.order(-1);
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		// TODO Auto-generated method stub
+		// addResourceHandler의 패턴이 들어오면
+		// addResourceLocation의 값에 설정된 리소스를 사용한다.
+		// setCacheControl은 캐시 값 설정이며 다른 설정 값들을 사용할 수도 있다.
+		registry.addResourceHandler("/mobile/**")
+				.addResourceLocations("classpath:/test/")
+				.setCacheControl(CacheControl.maxAge(10, TimeUnit.MINUTES));
 	}
 
 //	@Override
 //	public void addFormatters(FormatterRegistry registry) {
 //		registry.addFormatter(new PersonFormatter());
 //	}
+	
+	
 	
 	
 }
--- src/test/java/me/pulpury/demobootweb/SampleControllerTest.java
+++ src/test/java/me/pulpury/demobootweb/SampleControllerTest.java
@@ -3,12 +3,16 @@ package me.pulpury.demobootweb;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -45,6 +49,16 @@ public class SampleControllerTest {
 					.param("id", savedPerson.getId().toString()))
 				.andDo(print())
 				.andExpect(content().string("hello taeju"));
+	}
+	
+	@Test
+	public void helloStatic() throws Exception {
+		this.mockMvc.perform(get("/mobile/index.html"))
+					.andDo(print())
+					.andExpect(status().isOk())
+//					.andExpect(content().string(Matchers.containsString("Hello Mobile")))
+					.andExpect(content().string(Matchers.containsString("Hello Mobile TEST")))
+					.andExpect(header().exists(HttpHeaders.CACHE_CONTROL));
 	}
 
 }
